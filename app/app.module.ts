b2cedcdad1e19b8ad2d6279f8587e41e5ee7847e
ngVietTuan <105453139+ngVietTuan@users.noreply.github.com>
2023-03-30 22:26:57 +0700

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { APP_BOOTSTRAP_LISTENER, NgModule } from '@angular/core';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee.list/employee.list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeListComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
